45c0a12dac73137cc8287b9e5b16c63813986072
fufuisme <75672017+fufuisme@users.noreply.github.com>
2020-12-21 12:27:18 +0800

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@ def page_4():
         import Tkinter as tk
     except:
         import tkinter as tk
-
+    
     rec = tk.Tk()
     rec.title("剩菜小幫手")  # 此應用程式的名字
     rec.geometry('1500x750')
@@ -15,7 +15,7 @@ def page_3():
         import Tkinter as tk
     except:
         import tkinter as tk
-
+    
     rec = tk.Tk()
     rec.title("剩菜小幫手")  # 此應用程式的名字
     rec.geometry('1500x750')
@@ -23,13 +23,13 @@ def page_3():
     l=tk.Label(rec ,bg='RosyBrown' ,width=55 ,height=2 ,font=('Courier New', 30) ,text='想要看到甚麼樣的食譜呢?' )
     l.pack()
 
-    botton1=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20) ,text='越夯越好' ,indicatoron=False)
+    botton1=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20) ,text='越夯越好' ,indicatoron=False)  ###command= 按讚數排
     botton1.pack()
-    botton2=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20) ,text='快速上菜' ,indicatoron=False)
+    botton2=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20) ,text='快速上菜' ,indicatoron=False)  ###command= 按製作時間排
     botton2.pack()
-    botton3=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20), text='最新食譜', indicatoron=False)
+    botton3=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 20), text='最新食譜', indicatoron=False)  ###command= 按新舊排
     botton3.pack()
-
+    
     """
     換頁
     """
@@ -38,15 +38,15 @@ def page_3():
         page_4()
     nextpagebtn = tk.Button(rec, text="下一步", width=25 ,height=1, font=('Courier New', 18), command=commandthings)
     nextpagebtn.place(x=450, y=500)
-
-    rec.mainloop()
+    
+    rec.mainloop() 
 
 def page_2():
     try:
         import Tkinter as tk
     except:
-        import tkinter as tk
-
+        import tkinter as tk    
+        
     rec = tk.Tk()
     rec.title("剩菜小幫手")  # 此應用程式的名字
     rec.geometry('1500x750')
@@ -60,18 +60,18 @@ def page_2():
     """
     blank
     """
-    dish1 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish1 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 要處理的菜
     dish1.place(x=130, y=200)
-    dish2 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish2 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 要處理的菜
     dish2.place(x=130, y=300)
-    dish3 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish3 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 要處理的菜
     dish3.place(x=130, y=400)
 
-    dish1 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish1 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 不吃的菜
     dish1.place(x=770, y=200)
-    dish2 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish2 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 不吃的菜
     dish2.place(x=770, y=300)
-    dish3 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)
+    dish3 = tk.Entry(rec, show=None, font=('Courier New', 18), width=25)  ### command= 不吃的菜
     dish3.place(x=770, y=400)
     """
     換頁
@@ -81,15 +81,15 @@ def page_2():
         page_3()
     nextpagebtn = tk.Button(rec, text="下一步", width=25 ,height=1, font=('Courier New', 18), command=commandthings)
     nextpagebtn.place(x=450, y=500)
-
+    
     rec.mainloop()
 
-def page_1():
+def page_1():   
     try:
         import Tkinter as tk
     except:
-        import tkinter as tk
-
+        import tkinter as tk    
+        
     rec = tk.Tk()
     rec.title("剩菜小幫手")  # 此應用程式的名字
     rec.geometry('1500x750')
@@ -100,9 +100,9 @@ def page_1():
     l=tk.Label(rec ,bg='aliceblue' ,width=75 ,height=2 ,font=('Courier New', 30) ,text='今晚我想來點......' )
     l.pack()
 
-    botton1=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 18) ,text='湊一湊就上桌',indicatoron=False)
+    botton1=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 18) ,text='湊一湊就上桌',indicatoron=False)  ### command= 剩越少越好
     botton1.pack()
-    botton2=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 18) ,text='幫我盡可能處理掉他們 即使要付出代價',indicatoron=False)
+    botton2=tk.Radiobutton(rec ,height=1 ,font = ('Courier New', 18) ,text='幫我盡可能處理掉他們 即使要付出代價',indicatoron=False)  ### command= 處理越多越好
     botton2.pack()
 
     """
